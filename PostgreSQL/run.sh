ad220b6c8e82ffa11f4c0ac1ea7dc28874f5613c
Bruce Stephenson <bruce@brucejs.com>
2020-10-09 19:33:35 -0500

    Add PostgreSQL

---
+++ PostgreSQL/run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+########################################
+# Run command for PostgreSQL
+# Globals:
+#   POSTGRES_PASSWORD
+########################################
+docker container run                           \
+  --detach                                     \
+  --env POSTGRES_PASSWORD="$POSTGRES_PASSWORD" \
+  --name postgres                              \
+  --publish 5432                               \
+  --restart always                             \
+  --volume postgres:/var/lib/postgresql/data   \
+  postgres:12
