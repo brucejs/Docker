b7519c74779cc758dbaa6637f375c98a36150833
Bruce Stephenson <bruce@brucejs.com>
2020-10-14 17:25:04 -0500

    Add Cachet to PHP directory

---
+++ PHP/Cachet/run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+########################################
+# Run command for php-cachet.
+# Globals:
+#   None
+# Depends:
+#   postgres
+# Notes:
+#   You'll need a front-end proxy to pass requests to this container's
+#   port 9000.
+########################################
+docker container run                 \
+  --detach                           \
+  --link postgres                    \
+  --name php-cachet                  \
+  --publish 9000                     \
+  --restart always                   \
+  --volume cachet:/var/www/cachet:rw \
+  sharkmoji/php-cachet
