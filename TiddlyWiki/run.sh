61bc4cb243fb7209540163b1e79874eae4187ea2
Bruce Stephenson <bruce@brucejs.com>
2020-11-21 21:12:48 -0600

    Add TiddlyWiki

---
+++ TiddlyWiki/run.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+#
+########################################
+# Run command for tiddlywiki
+# Globals:
+#   None
+# Depends:
+#   None
+########################################
+docker container run \
+  --detach \
+  --name tiddlywiki \
+  --publish 8080 \
+  --restart always \
+  --volume /etc/localtime:/etc/localtime:ro \
+  --volume tiddlywiki:/wiki:rw \
+  sharkmoji/tiddlywiki
