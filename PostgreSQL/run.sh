d7b31b834cc6be9eee9c5a64563bc670f0d2c408
Bruce Stephenson <bruce@brucejs.com>
2020-11-23 22:21:08 +0000

    Nitpicky continuation line styling

--- PostgreSQL/run.sh
+++ PostgreSQL/run.sh
@@ -7,12 +7,12 @@
 # Depends:
 #   None
 ########################################
-docker container run                           \
-  --detach                                     \
-  --env POSTGRES_PASSWORD="$POSTGRES_PASSWORD" \
-  --name postgres                              \
-  --publish 5432                               \
-  --restart always                             \
-  --volume /etc/localtime:/etc/localtime:ro    \
+docker container run                            \
+  --detach                                      \
+  --env POSTGRES_PASSWORD="$POSTGRES_PASSWORD"  \
+  --name postgres                               \
+  --publish 5432                                \
+  --restart always                              \
+  --volume /etc/localtime:/etc/localtime:ro     \
   --volume postgres:/var/lib/postgresql/data:rw \
   postgres:12
