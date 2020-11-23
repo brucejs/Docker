303eca33ad45e1acd3ce3196a255d3b93bd8cc8f
Bruce Stephenson <bruce@brucejs.com>
2020-11-23 23:12:47 +0000

    Enable -h flag for MariaDB run script
    Enable binding 3306 to the host when needed on the fly by passing
    -h flag to run command.

--- MariaDB/run.sh
+++ MariaDB/run.sh
@@ -7,12 +7,33 @@
 # Depends:
 #   None
 ########################################
-docker container run                               \
-  --detach                                         \
-  --env MYSQL_ROOT_PASSWORD="$MYSQL_ROOT_PASSWORD" \
-  --name mariadb                                   \
-  --publish 3306                                   \
-  --restart always                                 \
-  --volume /etc/localtime:ro                       \
-  --volume mariadb:/var/lib/mysql:rw               \
-  mariadb:10
+HOST=false
+
+while getopts 'f' flag; do
+  case "${flag}" in
+    h) HOST=true ;;
+    *) error "Unexpected option ${flag}" ;;
+  esac
+done
+
+if $HOST; then
+  docker container run                               \
+    --detach                                         \
+    --env MYSQL_ROOT_PASSWORD="$MYSQL_ROOT_PASSWORD" \
+    --name mariadb                                   \
+    --publish 3306:3306                              \
+    --restart always                                 \
+    --volume /etc/localtime:ro                       \
+    --volume mariadb:/var/lib/mysql:rw               \
+    mariadb:10
+else 
+  docker container run                               \
+    --detach                                         \
+    --env MYSQL_ROOT_PASSWORD="$MYSQL_ROOT_PASSWORD" \
+    --name mariadb                                   \
+    --publish 3306                                   \
+    --restart always                                 \
+    --volume /etc/localtime:ro                       \
+    --volume mariadb:/var/lib/mysql:rw               \
+    mariadb:10
+fi
