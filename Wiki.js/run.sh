0668d99f4fc814abb99fc2a057a27da9e8a6cb8e
Bruce Stephenson <bruce@brucejs.com>
2020-11-23 21:43:13 +0000

    Mount host's /etc/localtime

--- Wiki.js/run.sh
+++ Wiki.js/run.sh
@@ -7,17 +7,18 @@
 # Depends:
 #   postgres
 ########################################
-docker container run                  \
-  --detach                            \
-  --env DB_HOST="postgres"            \
-  --env DB_NAME="wiki"                \
-  --env DB_PASS="$DB_PASS"            \
-  --env DB_PORT=5432                  \
-  --env DB_TYPE="postgres"            \
-  --env DB_USER="wiki"                \
-  --link postgres                     \
-  --name wiki                         \
-  --publish 3000                      \
-  --restart always                    \
-  --volume wiki:/wiki/data/content:rw \
+docker container run                        \
+  --detach                                  \
+  --env DB_HOST="postgres"                  \
+  --env DB_NAME="wiki"                      \
+  --env DB_PASS="$DB_PASS"                  \
+  --env DB_PORT=5432                        \
+  --env DB_TYPE="postgres"                  \
+  --env DB_USER="wiki"                      \
+  --link postgres                           \
+  --name wiki                               \
+  --publish 3000                            \
+  --restart always                          \
+  --volume /etc/localtime:/etc/localtime:ro \
+  --volume wiki:/wiki/data/content:rw       \
   requarks/wiki
