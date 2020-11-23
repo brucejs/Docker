4051cb56d81c4070379b30a22c31c447ac60bd66
Bruce Stephenson <bruce@brucejs.com>
2020-11-23 22:23:51 +0000

    Nitpicky styling

--- Redis/run.sh
+++ Redis/run.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 #
 ########################################
-# Run command for redis
+# Run command for Redis
 # Globals:
 #   REDIS_HOST_PASSWORD
 # Depends:
 #   None
 ########################################
-docker container run      \
-  --detach                \
-  --name redis            \
-  --publish 6379          \
-  --restart always        \
+docker container run                        \
+  --detach                                  \
+  --name redis                              \
+  --publish 6379                            \
+  --restart always                          \
   --volume /etc/localtime:/etc/localtime:ro \
-  --volume redis:/data:rw \
+  --volume redis:/data:rw                   \
   redis:6 redis-server --requirepass "$REDIS_HOST_PASSWORD"
