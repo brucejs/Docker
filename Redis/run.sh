6edafaad79092bbf48db5c54dceab9cc4fe53ecd
Bruce Stephenson <bruce@brucejs.com>
2020-11-23 19:53:09 +0000

    Publish default Redis port

--- Redis/run.sh
+++ Redis/run.sh
@@ -10,6 +10,7 @@
 docker container run \
   --detach \
   --name redis \
+  --publish 6379 \
   --restart always \
   --volume redis:/data:rw \
   redis:6 redis-server --requirepass "$REDIS_HOST_PASSWORD"
